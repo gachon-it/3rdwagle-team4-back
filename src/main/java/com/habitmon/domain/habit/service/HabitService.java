d83b1923b2ef9f65161743e3a4d6f2e77bc6b726
kimchaeyeon <kelly021128@naver.com>
2025-02-22 01:56:23 +0900

    feat: 습관 체크 count증감 및 완료여부

--- src/main/java/com/habitmon/domain/habit/service/HabitService.java
+++ src/main/java/com/habitmon/domain/habit/service/HabitService.java
@@ -1,5 +1,6 @@
 package com.habitmon.domain.habit.service;
 
+import com.habitmon.domain.habit.api.dto.request.HabitCountRequest;
 import com.habitmon.domain.habit.domain.Habit;
 import com.habitmon.domain.habit.api.dto.request.HabitCreateRequest;
 import com.habitmon.domain.habit.api.dto.response.HabitCreateResponse;
@@ -10,6 +11,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import com.habitmon.domain.habit.exception.HabitNotFoundException;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 @Service
@@ -54,5 +58,26 @@ public class HabitService {
                 habit.getComplete(),
                 habit.getCount()
         );
+    }
+
+    public void updateHabit(HabitCountRequest request){
+        Habit habit = habitRepository.findById(request.habitId()).orElseThrow(HabitNotFoundException::new);
+        long daysBetween = ChronoUnit.DAYS.between(habit.getStartDate(), habit.getEndDate());
+        int newCount = habit.getCount();
+        if(request.isCheked()){
+            newCount ++;
+        } else{
+            newCount --;
+        }
+        habit = habit.toBuilder()
+                .count(newCount)
+                .build();
+
+        if(daysBetween == newCount){
+            habit = habit.toBuilder()
+                    .complete(true)
+                    .build();
+        }
+        habitRepository.save(habit);
     }
 }
